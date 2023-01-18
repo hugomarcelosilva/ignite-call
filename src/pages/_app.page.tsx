2e2e9a0beb6b4e61d6d311cb7f408e19209c5f9b
Hugo Silva <hugo.marcelo@clevertech.biz>
2023-01-18 11:41:01 -0300

    first commit

---
+++ src/pages/_app.page.tsx
@@ -0,0 +1,32 @@
+import '../lib/dayjs'
+
+import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
+import { DefaultSeo } from 'next-seo'
+
+import { globalStyles } from '../styles/global'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '../lib/react-query'
+
+globalStyles()
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'pt_BR',
+            url: 'https://ignite-call.ct.com.br/',
+            siteName: 'Ignite Call',
+          }}
+        />
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
+  )
+}
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,43 @@
+import { Heading, Text } from '@ignite-design-system/react'
+import { NextSeo } from 'next-seo'
+
+import { Container, Hero, Preview } from './styles'
+
+import previewImage from '../../assets/app-preview.png'
+import Image from 'next/image'
+import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+
+export default function Home() {
+  return (
+    <>
+      <NextSeo
+        title="Descomplique sua agenda | Ignite Call"
+        description="Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre."
+      />
+      <Container>
+        <Hero>
+          <Heading as="h1" size="4xl">
+            Agendamento descomplicado
+          </Heading>
+          <Text size="xl">
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre.
+          </Text>
+
+          <ClaimUsernameForm />
+        </Hero>
+
+        <Preview>
+          <Image
+            src={previewImage}
+            height={400}
+            quality={100}
+            priority
+            alt="Calendário simbolizando aplicação em funcionamento"
+          />
+        </Preview>
+      </Container>
+    </>
+  )
+}
